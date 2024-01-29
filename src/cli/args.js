32f3ae4397716ba69ffa5b421359e8eea89e0376
Pavel Hancharou <pavel.hancharou@itechart-group.com>
2024-01-29 15:20:05 +0100

    feat: implement cli operations

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,17 @@
+import * as process from 'node:process';
+
 const parseArgs = () => {
-    // Write your code here 
+  const argsData = process.argv.slice(2);
+  const result = [];
+
+  for (let i = 0; i < argsData.length; i += 2) {
+    const key = argsData[i].replace('--', '');
+    const value = argsData[i + 1];
+
+    result.push(`${key} is ${value}`);
+  }
+
+  console.info(result.join(', ') || 'Not found');
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,18 @@
+import * as process from 'node:process';
+
 const parseEnv = () => {
-    // Write your code here 
+  const prefix = 'RSS_';
+  const envData = process.env;
+
+  const results = [];
+
+  for (const [envKey, envValue] of Object.entries(envData)) {
+    if(envKey.startsWith(prefix)){
+      results.push(`${envKey}=${envValue}`);
+    }
+  }
+
+  console.info(results.join('; ') || 'Not found');
 };
 
 parseEnv();
