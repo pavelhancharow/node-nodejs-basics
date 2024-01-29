da890a2f8f8959fdbe46ff172ad8d28e3175eb82
Pavel Hancharou <pavel.hancharou@itechart-group.com>
2024-01-29 15:33:25 +0100

    feat: implement cp operation

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,24 @@
+import * as path from 'node:path';
+import * as process from 'node:process';
+import { spawn } from 'node:child_process';
+
+const filePath = path.join(path.resolve(), 'src', 'cp', 'files', 'script.js');
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const childProcess = await spawn('node', [filePath, ...args], { stdio: ['pipe', 'pipe', 'pipe', 'ipc'] });
+
+  childProcess.stdout.pipe(process.stdout);
+  process.stdin.pipe(childProcess.stdin);
+
+  childProcess.on('exit', (code) => {
+    process.exit(code);
+  });
+
+  childProcess.on('error', (error) => {
+    console.error(error.toString());
+    process.exit(1);
+  });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess([...process.argv]);
