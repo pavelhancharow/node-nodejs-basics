d5bb74dc98c3c52444368f72a221c09e080edbd8
Pavel Hancharou <pavel.hancharou@itechart-group.com>
2024-01-29 15:28:47 +0100

    feat: implement hash operation

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,24 @@
+import * as fs from 'node:fs'
+import * as crypto from 'node:crypto';
+import * as path from 'node:path';
+import * as process from 'node:process';
+import { pipeline } from 'node:stream/promises';
+
+const filePath = path.join(path.resolve(), 'src', 'hash', 'files', 'fileToCalculateHashFor.txt');
+
 const calculateHash = async () => {
-    // Write your code here 
+  const hash = crypto.createHash('sha256', { encoding: 'hex' });
+  const readStream = fs.createReadStream(filePath);
+
+  hash.on('end', () => {
+    process.stdout.write('\n');
+  });
+
+  try {
+    await pipeline(readStream, hash, process.stdout);
+  } catch (error) {
+    console.error(error.toString());
+  }
 };
 
 await calculateHash();
